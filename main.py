d13dd8c2b6b6c4672ecf5df6b4edfa787b93be1b
dashixiong <774392850@qq.com>
2026-07-07 19:47:41 +0800

    添加print_hello

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi 铁昭123, {name}')  # 按 Ctrl+F8 切换断点。
 
+def print_hello(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hello 靓仔, {name}')
+
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hello('靓仔')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
